87eccef2e62a16e88e3b636641cbfbadb9c98a7b
larisa33 <muntoiu.larisa@gmail.com>
2023-09-26 21:03:51 +0300

    readline

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = Console.ReadLine();
+            Console.WriteLine("textul este: " + text);
         }
     }
 }
