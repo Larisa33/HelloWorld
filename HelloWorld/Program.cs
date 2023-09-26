5a0547da7c3fe79259b247a432427b29813c1032
larisa33 <muntoiu.larisa@gmail.com>
2023-09-26 21:32:00 +0300

    demo reading with conversion encapsulated in a function

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,11 +1,47 @@
-﻿namespace HelloWorld
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string text = Console.ReadLine();
-            Console.WriteLine("textul este: " + text);
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            int age = ReadNumberFrmKeyboard("Please enter your age: ");
+            Console.WriteLine("Hello, " + name + ", your age is: " + age);
+
+            int year = ReadNumberFrmKeyboard("Please enter current year: ");
+            Console.WriteLine("current year is" + year);
+
+
+            static int ReadNumberFrmKeyboard(string eticheta) 
+            {
+                bool canConvert = false;
+                while(!canConvert)
+                {
+                    Console.Write(eticheta);
+                    string text = Console.ReadLine();
+
+                    canConvert = int.TryParse(text, out int result);
+
+
+                    if (canConvert)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The text '" + text + "' dosen't represent a number.");
+                    }
+
+                }
+
+                return 0;
+
+            }
+
         }
     }
 }
